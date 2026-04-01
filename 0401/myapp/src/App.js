698483f54e806be518f39ed9a7e160feda1e6244
kyksc <endwl99@naver.com>
2026-04-01 11:55:58 +0900

    0401 수업 내용

---
+++ 0401/myapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyComponent from './MyComponent';
+function App() {
+  return (
+    <div>
+    <MyComponent name="HTML" />
+    <MyComponent name="Javascipt" />
+    <MyComponent name="React" />
+    </div>
+  );
+  
+}
+export default App;
